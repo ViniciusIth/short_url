9b4025fe40d3dd137aef30b0f0f9d5c776920fc5
ViniciusIth <vini.ithalo@hotmail.com>
2023-11-20 23:15:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project short_url
+
+A simple url shortener written in go + htmx.
+
---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"short_url/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic("cannot start server")
+	}
+}
